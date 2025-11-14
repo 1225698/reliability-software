bda5e730f546cb4be82e727088d75f64ed0d33f5
chenjinwen5412 <2426786359@qq.com>
2025-11-14 16:18:52 +0800

    加入可靠性建模页面标签

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Calculator from '../views/Calculator.vue'
+import Modeling from '../views/Modeling.vue'
 import Results from '../views/Results.vue'
 import Allocation from '../views/Allocation.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
+  { path: '/modeling', name: 'Modeling', component: Modeling },
   { path: '/calculator', name: 'Calculator', component: Calculator },
   { path: '/results', name: 'Results', component: Results },
   { path: '/allocation', name: 'Allocation', component: Allocation }
---
+++ src/views/Modeling.vue
@@ -0,0 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div></div>
+</template>
+
+<script setup>
+// 空白页面：可靠性建模
+</script>
